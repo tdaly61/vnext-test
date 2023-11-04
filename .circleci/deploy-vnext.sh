69e469ec0a3f569f4ff7656df510c9f445c854ce
Tom Daly <tdaly61@gmail.com>
2023-11-04 10:00:50 +0000

    setting up to test vnext deployment

---
+++ .circleci/deploy-vnext.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+echo "who am i" 
+who am i 
+
+echo "make the vnext directory"
+mkdir vnext 
+cd vnext 
+pwd 
+
+echo "clone platform-shared-tools "
+git clone --branch k8s_working_branch https://github.com/mojaloop/platform-shared-tools.git
+
+
