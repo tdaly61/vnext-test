7e5579c31016bc6e3e6d156d26102b3dbbbc281f
Tom Daly <tdaly61@gmail.com>
2023-11-04 10:18:13 +0000

    try running mini-loop-k8s.sh in pipeline

--- .circleci/mini-loop-ci.sh
+++ .circleci/mini-loop-ci.sh
@@ -9,6 +9,8 @@ cd vnext
 pwd 
 
 echo "clone platform-shared-tools "
-git clone --branch k8s_working_branch https://github.com/mojaloop/platform-shared-tools.git
+git clone --branch k8s_working_br https://github.com/mojaloop/platform-shared-tools.git
 
 ls 
+
+./platform-shared-tools/packages/installer/mini-loop/mini-loop-k8s.sh -m install
